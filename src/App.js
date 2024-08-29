323b71997df63a12662450fa84644af548d8b17e
Joy khulbe <khulbejoy.jk@gmail.com>
2024-08-29 14:07:07 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import { React } from "react";
-import "./App.css";
+import React from "react";
 import Navbar from "./components/Navbar";
+import "./App.css";
+import Home from "./components/pages/Home";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Services from "./components/pages/Services";
+import Products from "./components/pages/Products";
+import SignUp from "./components/pages/SignUp";
 
-function App() {
-  return (
-    <>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" exact />
-        </Routes>
-      </Router>
-    </>
-  );
-}
+const routes = (
+  <Router>
+    <Navbar />
+    <Routes>
+      <Route path="/" exact element={<Home />} />
+      <Route path="/services" element={<Services />} />
+      <Route path="/products" element={<Products />} />
+      <Route path="/sign-up" element={<SignUp />} />
+    </Routes>
+  </Router>
+);
+
+const App = () => {
+  return <div>{routes}</div>;
+};
 
 export default App;
